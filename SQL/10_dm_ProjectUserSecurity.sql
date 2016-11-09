bcbe377629d7281431afff3dbc777e9e312eddcb
Blackbaud-AlexShchetkov <Alex.Shchetkov@blackbaud.com>
2016-11-09 16:55:12 -0500

    Giving every user access to at least 1 fake account and project. Necessary for our provisioning to work.

--- SQL/10_dm_ProjectUserSecurity.sql
+++ SQL/10_dm_ProjectUserSecurity.sql
@@ -7,6 +7,16 @@ select
 	GoodData_Attr(UsersId)  as "UserId",
 	GoodData_Attr(ProjectId) as "ProjectId"
 from stg_csv_ProjectUserSecurity_merge t
+
+union all
+
+select
+	${TRANSFORM_ID['TRANSFORM_ID']} as _sys_transform_id,
+	u.TenantId as "TenantId",
+	 GoodData_Attr(UserId||'#'||-1)  as "ProjectUserSecurityId"
+	,GoodData_Attr(UserId)  as "UserId"
+	,GoodData_Attr(-1) as "ProjectId"
+from stg_csv_User_merge u;
 ;
 INSERT INTO _sys_transform_id (id,entity,ts_start,ts_end) VALUES (${TRANSFORM_ID['TRANSFORM_ID']},'dm_ProjectUserSecurity',null,now());
 select analyze_statistics('dm_ProjectUserSecurity')
