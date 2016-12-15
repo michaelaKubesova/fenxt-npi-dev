5f245a3c36d6418100786d773586fbc9fbc82eb5
Blackbaud-AlexShchetkov <Alex.Shchetkov@blackbaud.com>
2016-12-15 13:45:28 -0500

    Just some more work in trying to get account segment stuff to work.

--- workspace_etl/graph/sql/dm_AccountSegments.sql
+++ workspace_etl/graph/sql/dm_AccountSegments.sql
@@ -1,16 +1,6 @@
-select 1 as AccountSegmentId, AccountSegment1Name as AccountSegmentName from stg_csv_Tenant_merge where AccountSegment1Name != '' and TenantId = '${TenantId}'
-union all
-select 2 as AccountSegmentId, AccountSegment2Name as AccountSegmentName from stg_csv_Tenant_merge where AccountSegment2Name != '' and TenantId = '${TenantId}'
-union all
-select 3 as AccountSegmentId, AccountSegment3Name as AccountSegmentName from stg_csv_Tenant_merge where AccountSegment3Name != '' and TenantId = '${TenantId}'
-union all
-select 4 as AccountSegmentId, AccountSegment4Name as AccountSegmentName from stg_csv_Tenant_merge where AccountSegment4Name != '' and TenantId = '${TenantId}'
-union all
-select 5 as AccountSegmentId, AccountSegment5Name as AccountSegmentName from stg_csv_Tenant_merge where AccountSegment5Name != '' and TenantId = '${TenantId}'
-union all
-select 6 as AccountSegmentId, AccountSegment6Name as AccountSegmentName from stg_csv_Tenant_merge where AccountSegment6Name != '' and TenantId = '${TenantId}'
-union all
-select 7 as AccountSegmentId, AccountSegment7Name as AccountSegmentName from stg_csv_Tenant_merge where AccountSegment7Name != '' and TenantId = '${TenantId}'
-union all
-select 8 as AccountSegmentId, AccountSegment8Name as AccountSegmentName from stg_csv_Tenant_merge where AccountSegment8Name != '' and TenantId = '${TenantId}' 
-;	
+select Gooddata_attr(T.GL7ACCOUNTSID||'#'||T.SEQUENCE) as AccountSegmentId
+,T.GL7ACCOUNTSID as AccountId
+	,T.SEQUENCE as SegmentNumber
+	,T.SegmentName as SegmentName
+	,T.KEYVALUE as AccountSegmentValue from stg_csv_AccountSegmentValue_merge T where TenantId = '${TenantId}';
+
