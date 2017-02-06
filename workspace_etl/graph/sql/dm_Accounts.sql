c6d335c1b3671fc39dbed0de065d6700f2385ba9
Blackbaud-AlexShchetkov <Alex.Shchetkov@blackbaud.com>
2017-02-06 10:22:33 -0500

    getting rid of segments from dm_accounts

--- workspace_etl/graph/sql/dm_Accounts.sql
+++ workspace_etl/graph/sql/dm_Accounts.sql
@@ -20,14 +20,6 @@ select
  "ChangedByUserName",
  "ChangedByUserNameLabel",
  "AccountDateId",
- "AccountSegment1",
- "AccountSegment2",
- "AccountSegment3",
- "AccountSegment4",
- "AccountSegment5",
- "AccountSegment6",
- "AccountSegment7",
- "AccountSegment8",
  "Dummy"
 from dm_Accounts
 where TenantId = '${TenantId}'
