40c95dba170e574e6d2f47e02ca2973970204083
Blackbaud-AlexShchetkov <Alex.Shchetkov@blackbaud.com>
2017-03-28 11:08:19 -0400

    Added changes to support all url drill ins that we need

--- workspace_etl/graph/sql/dm_Invoices_attr.sql
+++ workspace_etl/graph/sql/dm_Invoices_attr.sql
@@ -5,7 +5,8 @@ select
  Description as "DescriptionLabel",
  "StatusTranslation",
  "AddedByUserName",
- "AddedByUserNameLabel"
+ "AddedByUserNameLabel",
+ GoodData_Attr('{"state":"payables.invoice.detail","id":'||InvoiceId||'}') "InvoiceNumberHyperlink"
 from dm_Invoices_attr
 where TenantId = '${TenantId}'
 and _sys_transform_id = '${DM_INVOICES_ATTR_TRANSFORM_ID}'
