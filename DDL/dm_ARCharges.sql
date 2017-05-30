ca3b6a2a1cbcdb6f60a0c55f7267e1c6cffc4147
Vaclav Rek <vaclav.rek@gooddata.com>
2017-05-30 14:40:27 +0200

    date format handling

--- DDL/dm_ARCharges.sql
+++ DDL/dm_ARCharges.sql
@@ -11,9 +11,9 @@ ARChargeItemDescription,
 ARChargeAmount,
 ARChargeBalance,
 ARClientId,
-datechanged,
-dateadded,
-postdate,
-duedate
+GoodData_Date(datechanged::date) datechanged,
+GoodData_Date(dateadded::date) dateadded,
+GoodData_Date(postdate::date) postdate,
+GoodData_Date(duedate::date) duedate
 from out_ARCharges 
  where _sys_transform_id = (select max(id) from _sys_transform_id where ts_end is not null and entity = 'dm_ARCharges');
