244e8a17e5005c63b97c3e3d19092354f5b687eb
vaclav <vaclav.rek@gooddata.com>
2018-07-25 13:35:17 +0200

    TransactionScenario increment

--- DDL/out_TransactionScenario.sql
+++ DDL/out_TransactionScenario.sql
@@ -1,15 +1,26 @@
+drop table if exists wrk_out_TransactionScenario;
+drop table if exists wrk_out_TransactionScenario_diff;
 drop table if exists out_TransactionScenario;
+
 CREATE TABLE out_TransactionScenario
 (
-    _sys_transform_id int NOT NULL encoding rle,
     TenantId varchar(255) encoding rle,
-    TransactionScenarioId varchar(255),
-    TransactionDistributionId varchar(255),
-    ScenarioId varchar(255),
-    FiscalPeriodId varchar(255),
-    AccountId varchar(255)
-)
- ORDER BY TenantId,
-          _sys_transform_id
+    TransactionDistributionId int,
+    ScenarioId varchar(255) ENCODING RLE,
+    FiscalPeriodId int ENCODING RLE,
+    AccountId int ENCODING RLE,
+    _sys_is_deleted boolean  ENCODING RLE,
+    _sys_updated_at timestamp ENCODING RLE
+)  ORDER BY TenantId,
+    TransactionDistributionId,
+    ScenarioId,
+    FiscalPeriodId,
+    AccountId,
+    _sys_is_deleted
 SEGMENTED BY hash(TenantId) ALL NODES
-PARTITION BY (_sys_transform_id);
+;
+
+create table wrk_out_TransactionScenario like out_TransactionScenario including projections;
+create table wrk_out_TransactionScenario_diff like out_TransactionScenario including projections;
+
+ALTER TABLE out_TransactionScenario ADD CONSTRAINT out_TransactionScenario_MK_PK PRIMARY KEY (tenantid, TransactionDistributionId, ScenarioId);
--- DDL/tmp_transaction_scenario.sql
+++ DDL/tmp_transaction_scenario.sql
@@ -1,33 +1,6 @@
-CREATE TABLE wrk_tmp_transaction_scenario
-(
-    AccountId int ENCODING RLE,
-    ScenarioId varchar(255) ENCODING RLE,
-    FiscalPeriodId int ENCODING RLE,
-    tenantid varchar(255) ENCODING RLE,
-    _sys_is_deleted boolean ENCODING RLE,
-    _sys_updated_at timestamp
-) ORDER BY tenantid,
-          AccountId,
-          FiscalPeriodId,
-          ScenarioId,
-          _sys_is_deleted
-SEGMENTED BY hash(tenantid) ALL NODES;
-
-CREATE TABLE wrk_tmp_transaction_scenario_diff
-(
-    AccountId int ENCODING RLE,
-    ScenarioId varchar(255) ENCODING RLE,
-    FiscalPeriodId int ENCODING RLE,
-    tenantid varchar(255) ENCODING RLE,
-    _sys_is_deleted boolean ENCODING RLE,
-    _sys_updated_at timestamp
-) ORDER BY tenantid,
-          AccountId,
-          FiscalPeriodId,
-          ScenarioId,
-          _sys_is_deleted
-SEGMENTED BY hash(tenantid) ALL NODES;
-
+drop table if exists wrk_tmp_transaction_scenario;
+drop table if exists wrk_tmp_transaction_scenario_diff;
+drop table if exists tmp_transaction_scenario;
 
 CREATE TABLE tmp_transaction_scenario
 (
@@ -36,12 +9,15 @@ CREATE TABLE tmp_transaction_scenario
     FiscalPeriodId int NOT NULL ENCODING RLE,
     tenantid varchar(255) NOT NULL ENCODING RLE,
     _sys_is_deleted boolean ENCODING RLE,
-    _sys_updated_at timestamp
+    _sys_updated_at timestamp  ENCODING RLE
 ) ORDER BY tenantid,
           AccountId,
           FiscalPeriodId,
           ScenarioId,
           _sys_is_deleted
 SEGMENTED BY hash(tenantid) ALL NODES;
+
+create table wrk_tmp_transaction_scenario like tmp_transaction_scenario including projections;
+create table wrk_tmp_transaction_scenario_diff like tmp_transaction_scenario including projections;
 
 ALTER TABLE tmp_transaction_scenario ADD CONSTRAINT tmp_transaction_scenario_PK PRIMARY KEY (tenantid, AccountId, FiscalPeriodId, ScenarioId);
